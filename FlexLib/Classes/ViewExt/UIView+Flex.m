18dfa52cddf0bf4821456468d9c3ef877fa0ff78
zhenglibao <zhenglibao@haizhi.com>
2017-12-09 16:33:29 +0800

    copyright

--- FlexLib/Classes/ViewExt/UIView+Flex.m
+++ FlexLib/Classes/ViewExt/UIView+Flex.m
@@ -4,10 +4,8 @@
  * All rights reserved.
  *
  * This source code is licensed under the MIT-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * LICENSE file in the root directory of this source tree.
  */
-
 
 #import "UIView+Flex.h"
 #import <objc/runtime.h>
