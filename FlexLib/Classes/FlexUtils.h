945bfbff5ad86c461a8c8ac7013cf0ba0c550fa9
zhenglibao <zhenglibao@haizhi.com>
2017-12-12 11:07:30 +0800

    调整宏定义位置

--- FlexLib/Classes/FlexUtils.h
+++ FlexLib/Classes/FlexUtils.h
@@ -18,6 +18,60 @@ typedef struct {
 #define FLEXSTR2INT(kvTable)    \
 NSString2Int(sValue,kvTable,sizeof(kvTable)/sizeof(NameValue))
 
+#define FLEXSET(propName)               \
+-(void)setFlex##propName:(NSString*)sValue
+
+//下面宏属性名称与UIView的属性名称保持一致
+// 声明颜色属性
+#define FLEXSETCLR(propName)              \
+FLEXSET(propName)                         \
+{                                         \
+self.propName = colorFromString(sValue);  \
+}
+
+// 声明浮点型属性
+#define FLEXSETFLT(propName)              \
+FLEXSET(propName)                         \
+{                                         \
+self.propName = [sValue floatValue];      \
+}
+
+// 声明整形属性
+#define FLEXSETINT(propName)              \
+FLEXSET(propName)                         \
+{                                         \
+self.propName = [sValue integerValue];    \
+}
+
+// 声明bool属性
+#define FLEXSETBOOL(propName)             \
+FLEXSET(propName)                         \
+{                                         \
+self.propName = String2BOOL(sValue);      \
+}
+
+// 声明double属性
+#define FLEXSETDBL(propName)              \
+FLEXSET(propName)                         \
+{                                         \
+self.propName = [sValue doubleValue];     \
+}
+// 声明字符串型属性
+#define FLEXSETSTR(propName)              \
+FLEXSET(propName)                         \
+{                                         \
+self.propName = sValue;                   \
+}
+
+// 声明枚举属性
+#define FLEXSETENUM(propName,table)       \
+FLEXSET(propName)                         \
+{                                         \
+NSInteger n=NSString2Int(sValue,          \
+table,sizeof(table)/sizeof(NameValue));   \
+self.propName = n;                        \
+}
+
 
 // 在表中查询s字符串，将其转换成int值
 int String2Int(const char* s,
