a77d4fc7a4db5e1bdb37b6b9efbe871a3a6b9ca5
zhenglibao <zhenglibao@haizhi.com>
2017-12-14 13:04:48 +0800

    增加例子

---
+++ Example/FlexLib/DemoFlexVC.m
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2017-present, zhenglibao, Inc.
+ * email: 798393829@qq.com
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "DemoFlexVC.h"
+
+@interface DemoFlexVC ()
+
+@end
+
+@implementation DemoFlexVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
