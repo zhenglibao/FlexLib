1b5edb9f794ebfac08b7c806164f9ac0a744a60c
zhenglibao <zhenglibao@haizhi.com>
2017-12-22 15:55:37 +0800

    完善swift demo

--- Example/FlexSwiftDemo/TestScrollVC.swift
+++ Example/FlexSwiftDemo/TestScrollVC.swift
@@ -24,16 +24,4 @@ class TestScrollVC: FlexBaseVC {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
